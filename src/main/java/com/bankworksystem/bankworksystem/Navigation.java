43efee6fa01f2a0d240e1c5f0caedd669502313d
Cybert47 <estebanhepa@unisabana.edu.co>
2023-11-25 19:24:29 -0500

    Views

---
+++ src/main/java/com/bankworksystem/bankworksystem/Navigation.java
@@ -0,0 +1,16 @@
+package com.bankworksystem.bankworksystem;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Navigation extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
