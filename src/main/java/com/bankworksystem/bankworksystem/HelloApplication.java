108fea2b9438bcfff6ddf150a886864a6f7d45f3
Cybert47 <estebanhepa@unisabana.edu.co>
2023-11-26 22:16:17 -0500

    icono

--- src/main/java/com/bankworksystem/bankworksystem/HelloApplication.java
+++ src/main/java/com/bankworksystem/bankworksystem/HelloApplication.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("initWindow.fxml"));
         Parent root = fxmlLoader.load();
 
+        URL imageUrl = HelloApplication.class.getResource("/images/logo.png");
+
+        Image icon = new Image(imageUrl.toExternalForm());
+        stage.getIcons().add(icon);
         Scene scene = new Scene(root, 700, 500);
         stage.setScene(scene);
 
